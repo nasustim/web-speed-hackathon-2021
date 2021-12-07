2559a5ce43fed6b1cfcd61cebdd8b351c49cf2e6
nasustim <mitsuhibino@gmail.com>
2021-12-08 00:27:03 +0900

    enable production build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,7 +31,6 @@ const config = {
       path.resolve(SRC_PATH, './index.jsx'),
     ],
   },
-  mode: 'none',
   module: {
     rules: [
       {
@@ -64,7 +63,6 @@ const config = {
       BUILD_DATE: new Date().toISOString(),
       // Heroku では SOURCE_VERSION 環境変数から commit hash を参照できます
       COMMIT_HASH: process.env.SOURCE_VERSION || '',
-      NODE_ENV: 'development',
     }),
     new MiniCssExtractPlugin({
       filename: 'styles/[name].css',
