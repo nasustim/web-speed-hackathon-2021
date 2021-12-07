ade2a89b4e9a71c7694b73f27ba110cd35464bdd
nasustim <mitsuhibino@gmail.com>
2021-12-08 02:37:18 +0900

    apply css minimizer

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
 
 const SRC_PATH = path.resolve(__dirname, './src');
@@ -78,6 +79,11 @@ const config = {
       path: false,
     },
   },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetPlugin(),
+    ]
+  }
 };
 
 module.exports = config;
