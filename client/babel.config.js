c3ffc1c3de02901a787ef8bc2ebffc3b3f980c69
nasustim <mitsuhibino@gmail.com>
2021-12-17 02:24:18 +0900

    detect target browser

--- client/babel.config.js
+++ client/babel.config.js
@@ -6,6 +6,11 @@ module.exports = {
         corejs: '3',
         modules: 'commonjs',
         useBuiltIns: 'usage',
+        targets: {
+          browsers: [
+           'last 1 Chrome version',
+          ]
+        }
       },
     ],
     [
